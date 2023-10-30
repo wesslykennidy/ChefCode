f94fcf0a4b63ae774a2a588e4609eb75c5d72971
wesslykennidy <wesslykennidy95@gmail.com>
2023-10-30 16:56:50 +0800

    Create ICM0013 (D-1112).py

---
+++ ICM0013 (D-1112).py
@@ -0,0 +1,13 @@
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    '''
+    count = 0
+    modulo_list = list()
+    for i in range(1,n+1):
+        if 2*(n%i) <= n:
+            modulo_list.append(i)
+    print(len(modulo_list))
+    '''
+    print(n)
+    
