12b0f55e21edd79ac6b611b38d340b26e607bd42
wesslykennidy <wesslykennidy95@gmail.com>
2023-11-17 10:40:12 +0800

    Create BNE_APT (D-280).py

---
+++ BNE_APT (D-280).py
@@ -0,0 +1,4 @@
+# cook your dish here
+bones_people, blood_people = map(int,input().split())
+bones_treats, blood_treats = map(int,input().split())
+print(bones_treats*bones_people + blood_treats*blood_people)
